bff44a11b7bb9ccd99414bdb514f7c508fcef2d9
moroball14 <daiki.mr.0330@gmail.com>
2019-12-12 20:33:39 +0900

    住所テーブルへの入力フォームを作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
   end
 
   def new
-    @event = Event.new()
+    @event = Event.new
+    @event.addresses.build
   end
 
   def create
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to :address
+  accepts_nested_attributes_for :address
 end
