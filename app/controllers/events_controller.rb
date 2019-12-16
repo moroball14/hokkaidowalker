0a9cd14d526bc4ce1ecfe11f5d8a7eb1ea0c28d9
moroball14 <daiki.mr.0330@gmail.com>
2019-12-16 20:07:06 +0900

    イベント登録が可能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,21 +13,21 @@ class EventsController < ApplicationController
   def create
     @address = Address.new(event_params[:address_attributes])
     @address.geocode
-    event_params[:address_attributes].merge!(latitude: @address.latitude, longitude: @address.longitude) # event_paramsを上書きして緯度経度を含んだ状態にしたい。けどmergeが上手くいかない。
-    # event_params[:address_attributes].geocode
-    @event = Event.new(event_params)
-    binding.pry
-    # if @address.geocode
-    #   if @address.valid?
-    #     @event.save
-    #   else
-    #     # 緯度でバリデーションかけて「北海道ではありません」とエラーメッセージ を返す
-    #   end
-    # else
-    #   # 「位置情報を取得できませんでした」とエラーメッセージを返す
-    # end
-    @event.save
-    redirect_to root_path
+    unless @address[:latitude] == nil
+    # binding.pry
+    # #   if @address.valid?
+      new_params = event_params
+      new_params[:address_attributes].merge!(latitude: @address.latitude, longitude: @address.longitude)
+      @event = Event.new(new_params)
+      @event.save
+    # #   else
+    # #     # 緯度でバリデーションかけて「北海道ではありません」とエラーメッセージ を返す
+    # #   end
+      redirect_to root_path
+    else
+      redirect_to new_event_path, alert: '位置情報を取得できませんでした'
+       # 「位置情報を取得できませんでした」とエラーメッセージを返す
+    end
   end
 
   def edit
@@ -43,5 +43,9 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event).permit(:name, :url, :start, :end, :category_id, address_attributes: [:postcode, :place, :place_building]).merge(user_id: current_user.id)
   end
+  # private
+  # def event_params(address_hash={})
+  #   params.require(:event).permit(:name, :url, :start, :end, :category_id, address_attributes: [:postcode, :place, :place_building]).merge(user_id: current_user.id, **address_hash)
+  # end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -6,7 +6,9 @@ class Address < ApplicationRecord
     uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+address.gsub(" ", "")+"&key=#{ENV['GOOGLE_MAP_API_KEY']}")
     res = HTTP.get(uri).to_s
     response = JSON.parse(res)
-    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
-    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+    unless response["status"] == "INVALID_REQUEST"
+      self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+      self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+    end
   end
 end
