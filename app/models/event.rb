74d86b92a0e4a7fdd8ec52e8d700dca38ec19bdc
moroball14 <daiki.mr.0330@gmail.com>
2019-12-18 10:39:19 +0900

    アソシエーションの定義

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,8 @@ class Event < ApplicationRecord
   belongs_to_active_hash :category
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
+  belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favoriting_users, through: :favorites, source: :user
 
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,4 @@
 class Favorite < ApplicationRecord
+  belongs_to :event, counter_cache: :likes_count
+  belongs_to :user
 end
