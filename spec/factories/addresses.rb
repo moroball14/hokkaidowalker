ad19348360da665ef9d7c741114a7cd26cb14558
moroball14 <daiki.mr.0330@gmail.com>
2019-12-22 12:26:25 +0900

    モデルのテストファイルを作成

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     postcode       { "062-0901" }
     place          { "北海道札幌市豊平区豊平一条一丁目1-2" }
     place_building { "コート豊平607" }
-    latitude       { "42.9393" }
-    longitude      { "143.187" }
+    latitude       { 42.9393 }
+    longitude      { 143.187 }
     event_id       { "1" }
   end
 end
