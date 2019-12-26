8ec2e31f6b8a6727e9c7ecab90b69bf3d9959c72
moroball14 <daiki.mr.0330@gmail.com>
2019-12-26 19:20:22 +0900

    不要なコメントを削除

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-# Rails.application.config.assets.precompile += %w( *.eot *.woff *.woff2 *.ttf *.svg *.otf *.png *.jpg *.jpeg *.gif application.css *.js )
