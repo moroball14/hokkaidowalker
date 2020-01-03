9d9896c61b877c960e57ccc79a9b5641edaf9382
moroball14 <daiki.mr.0330@gmail.com>
2020-01-03 14:19:06 +0900

    googleanalyticsの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  GA.tracker = <%= ENV['GOOGLE_ANALYTICS']%>
+
 end
